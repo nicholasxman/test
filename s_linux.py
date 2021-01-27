dc046df4fe1a72b0c4a44ce439f2cef94bae6f7b
nicholasxman <31949075+nicholasxman@users.noreply.github.com>
2021-01-27 14:42:58 +0800

    Update s_linux.py

--- s_linux.py
+++ s_linux.py
@@ -8,6 +8,7 @@ import time
 import random
 import uuid
 #gg
+#hhhh
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!再加上定时
 MY_USER_AGENT = [
 	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
